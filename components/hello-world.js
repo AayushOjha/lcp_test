19fde9e572adf7712294ce0f218872f1a3132ee5
Aayushojha <18egjcs024@gitjaipur.com>
2023-01-17 19:16:40 +0530

    initial

--- components/hello-world.js
+++ components/hello-world.js
@@ -1,9 +1,19 @@
-import styles from './hello-world.module.scss'
+import Image from 'next/image';
 
 export default function HelloWorld() {
+  const demo = (number) => {
+    return number * 1000;
+  };
+  demo();
   return (
-    <div className={styles.hello}>
-      Hello World, I am being styled using SCSS Modules!
+    <div>
+      <Image
+        loader={({ src }) => src}
+        src="https://images.unsplash.com/photo-1673893821298-3ff6b49990a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+        width={500}
+        height={500}
+        priority
+      />
     </div>
-  )
+  );
 }
---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {
+  images: {
+    domains: [
+      'images.thrillophilia.com',
+      'images.unsplash.com',
+      'res.cloudinary.com',
+      'localhost',
+    ],
+  },
+};
+
+module.exports = nextConfig;
